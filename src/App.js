c7a793678ee804c5a7d731bdff0d4e568377bc5c
kushagra <kushagratripathi7@gmail.com>
2022-08-06 15:38:18 +0530

    fix: app.css name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./app.css";
+import "./App.csss";
 import Card from "./components/Card";
 import Filter from "./components/Filter";
 import Input from "./components/Input";
